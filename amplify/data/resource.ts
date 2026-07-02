544ff0b9af163c6aa35dff1cd6bc671c3c937712
ammonkennedy <ammonkennedy@gmail.com>
2026-07-02 13:35:46 -0400

    Add other sample type to data model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -18,7 +18,7 @@ const schema = a.schema({
 
   Sample: a
     .model({
-      sampleType: a.enum(["water", "rock", "soil_sand"]),
+      sampleType: a.enum(["water", "rock", "soil_sand", "other"]),
       sampleId: a.string().required(),
       datasetId: a.id(),
       notes: a.string(),
